0fb995fdebfe60288427df96dd435623eabe7a88
William Chabla <w_chabla@hotmail.com>
2018-06-25 17:51:18 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,8 @@ class Hash
   def keys_of(*arguments)
     array_matching_keys = []
     
-    arguments.each do |key_lookup|
-      
+    arguments.each do |value_lookup|
+      if self.values.include?(value_lookup)
+        array_matching_keys << 
   end
 end
