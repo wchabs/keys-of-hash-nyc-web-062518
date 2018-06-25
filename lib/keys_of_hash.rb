33beb00c9e3aa6ff8f225e045e68376038f52ae1
William Chabla <w_chabla@hotmail.com>
2018-06-25 18:15:50 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array_matching_keys = []
+    
+    arguments.each do |value_lookup|
+      if self.values.include?(value_lookup)
+        self.each do |key, value|
+          if value == value_lookup
+            array_matching_keys << key
+          end
+        end
+      end
+    end
+    array_matching_keys
   end
 end
